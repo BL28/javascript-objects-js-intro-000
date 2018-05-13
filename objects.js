b9667956670ef2e0695c3f8b7d4292af1d68aa11
B L <pandabl28@hotmail.com>
2018-05-13 06:30:33 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -3,7 +3,7 @@ var playlist = {
   songTitle: 'Delicate'
 }
 
-function updatePlaylist (playlist, artistName, songTitle){
+function updatePlaylist(playlist, artistName, songTitle){
     playlist.assign({artistName}, {songTitle});
     return playlist;
 }
