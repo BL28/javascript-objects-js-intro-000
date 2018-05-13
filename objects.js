f6f8c9529b95d2cb468ea3a4499d9c7ca2cfc7c9
B L <pandabl28@hotmail.com>
2018-05-13 06:43:04 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,10 +1,10 @@
 var playlist = {'Taylor Swift':'Delicate'};
 
 function updatePlaylist(playlist, artistName, songTitle){
-   return playlist[artistName] = songTitle
+   return playlist[artistName] = songTitle;
 }
 
 function removeFromPlaylist(playlist, artistName){
-  delete playlist[artistName]
-  return playlist
+  delete playlist[artistName];
+  return playlist;
 }
