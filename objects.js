af004599f020ec51459055da9cabddb5973fcc91
B L <pandabl28@hotmail.com>
2018-05-13 06:40:17 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,8 +1,7 @@
 var playlist = {'Taylor Swift':'Delicate'};
 
 function updatePlaylist(playlist, artistName, songTitle){
-    playlist = {artistName, songTitle};
-    return playlist;
+   return playlist.assign({}, playlist, { [artistName]: songTitle})
 }
 
 function removeFromPlaylist(playlist, artistName){
